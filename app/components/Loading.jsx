775ca3850cae8dbb266b9ee87f9bf0a1fad35270
Marco Moeller <maaakooo@gmx.de>
2024-07-30 13:33:13 +0200

    feature: loading animation
    changed gif with "Loading ..." text in load screen ,animated the dots

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Loading = () => {
+  const [dotCount, setDotCount] = useState(1);
+
+  useEffect(() => {
+    const animateLoadingText = () => {
+      console.log("interval");
+      setDotCount((prevState) => (prevState + 1) % 5);
+    };
+    console.log("useEffect");
+    const interval = setInterval(() => {
+      animateLoadingText();
+    }, 500);
+  }, []);
+
   return (
     <div className="loading">
-      <img src="loading.gif" alt="Loading..." />
+      {/* <img src="loading.gif" alt="Loading..." /> */}
+      <p className="loading-text">Loading {`.`.repeat(dotCount)}</p>
     </div>
   );
 };
