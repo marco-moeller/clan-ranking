dc7b384d6c12548aa7d37148d0a9b8b0e9065f78
Marco Moeller <maaakooo@gmx.de>
2024-10-30 17:13:29 +0100

    hide firebase congfig
    moved firebase config to env file

--- database/config.js
+++ database/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDg6_5uVrT4rkS3HXQDxc3zgx9ynC3CIbk",
-  authDomain: "clan-ranking.firebaseapp.com",
-  projectId: "clan-ranking",
-  storageBucket: "clan-ranking.appspot.com",
-  messagingSenderId: "1039526407507",
-  appId: "1:1039526407507:web:024664eceeb33253cb45a6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.envNEXT_PUBLICP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.RNEXT_PUBLICFIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
